5d1f5aee3cbf4782ca1eaee4a2246f88e475c3c5
nowrap <nowrap@gmx.net>
2012-07-27 22:40:32 +0200

    bugfixing complete

--- pp_mailinglisten.js
+++ pp_mailinglisten.js
@@ -1,32 +1,34 @@
 $(document).ready(function() {
-	$.ajax({
-		type: "GET",
-		url: "stats.xml",
-		dataType: "xml",
-		cache: false,
-		success: function(xml) {
-			var hrefs = {};
- 			$("table.mailinglisten td:first-child a").each(function(idx, a) {
-				var href = $(a).attr("href");
-				href = href.replace(/cgi-bin\/mailman/, "mailman");
-				$(a).attr("href", href);
-				href = href.replace(/mailman/, "cgi-bin/mailman");
-				hrefs[href] = a;
-			});
+	if (typeof pp_mailinglisten !== "undefined") {
+		$.ajax({
+			type: "GET",
+			url: "/stats.php",
+			dataType: "xml",
+			cache: false,
+			success: function(xml) {
+				var hrefs = {};
+				$("table.mailinglisten td:first-child a").each(function(idx, a) {
+					var href = $(a).attr("href");
+					href = href.replace(/cgi-bin\/mailman/, "mailman");
+					$(a).attr("href", href);
+					href = href.replace(/mailman/, "cgi-bin/mailman");
+					hrefs[href] = a;
+				});
 
-			$(xml).find('list').each(function() {
-				var url = $(this).attr("url");
-				url = url.toLowerCase();
+				$(xml).find('list').each(function() {
+					var url = $(this).attr("url");
+					url = url.toLowerCase();
 
-				if (hrefs[url]) {
-					var members = $(this).attr("members");
-					$(hrefs[url]).parent().next().html(members);
-				}
-			});
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			console.log(textStatus);
-			console.log(jqXHR);
-		}
-	});
+					if (hrefs[url]) {
+						var members = $(this).attr("members");
+						$(hrefs[url]).parent().next().html(members);
+					}
+				});
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.log(textStatus);
+				console.log(jqXHR);
+			}
+		});
+	}
 });
